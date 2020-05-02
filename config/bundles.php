7281870bd2a76e0e5e6c90b0f3135b09ef51e8e3
Andrew Zhdanovskih <azh@crtweb.ru>
2020-05-02 10:44:40 +0500

    Fixes, packages and configuratins

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Andrew72ru\ChunkUploadBundle\ChunkUploadBundle::class => ['all' => true],
+    Oneup\FlysystemBundle\OneupFlysystemBundle::class => ['all' => true],
 ];
